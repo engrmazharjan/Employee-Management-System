805c6e9139305c06afdc2c0141f1e77c650416ca
Mazhar Jan <mazharj4565@gmail.com>
2022-09-06 10:29:57 +0500

    Initial project setup

---
+++ EmployeeManagement.Models/Department.cs
@@ -0,0 +1,8 @@
+﻿namespace EmployeeManagement.Models
+{
+    public class Department
+    {
+        public int  DepartmentId{ get; set; }
+        public string  DepartmentName{ get; set; }
+    }
+}
---
+++ EmployeeManagement.Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public Gender Gender { get; set; }
+        public Department Department { get; set; }
+        public string PhotoPath{ get; set; }
+    }
+}
