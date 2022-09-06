eec92254c82ecf5ded73e083393f16451c72b3d1
Mazhar Jan <mazharj4565@gmail.com>
2022-09-06 11:05:53 +0500

    Employee list blazor component

---
+++ EmployeeManagement.Web/Pages/EmployeeListBase.cs
@@ -0,0 +1,70 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace EmployeeManagement.Web.Pages
+{
+    public class EmployeeListBase:ComponentBase
+    {
+        public IEnumerable<Employee> Employees { get; set; }
+
+        protected override Task OnInitializedAsync()
+        {
+            LoadEmployees();
+            return base.OnInitializedAsync();
+        }
+
+
+        private void LoadEmployees()
+        {
+            Employee e1 = new Employee
+            {
+                EmployeeId = 1,
+                FirstName = "John",
+                LastName = "Hastings",
+                Email = "David@pragimtech.com",
+                DateOfBirth = new DateTime(1980, 10, 5),
+                Gender = Gender.Male,
+                Department = new Department { DepartmentId = 1, DepartmentName = "IT" },
+                PhotoPath = "images/john.png"
+            };
+
+            Employee e2 = new Employee
+            {
+                EmployeeId = 2,
+                FirstName = "Sam",
+                LastName = "Galloway",
+                Email = "Sam@pragimtech.com",
+                DateOfBirth = new DateTime(1981, 12, 22),
+                Gender = Gender.Male,
+                Department = new Department { DepartmentId = 2, DepartmentName = "HR" },
+                PhotoPath = "images/sam.jpg"
+            };
+
+            Employee e3 = new Employee
+            {
+                EmployeeId = 3,
+                FirstName = "Mary",
+                LastName = "Smith",
+                Email = "mary@pragimtech.com",
+                DateOfBirth = new DateTime(1979, 11, 11),
+                Gender = Gender.Female,
+                Department = new Department { DepartmentId = 1, DepartmentName = "IT" },
+                PhotoPath = "images/mary.png"
+            };
+
+            Employee e4 = new Employee
+            {
+                EmployeeId = 3,
+                FirstName = "Sara",
+                LastName = "Longway",
+                Email = "sara@pragimtech.com",
+                DateOfBirth = new DateTime(1982, 9, 23),
+                Gender = Gender.Female,
+                Department = new Department { DepartmentId = 3, DepartmentName = "Payroll" },
+                PhotoPath = "images/sara.png"
+            };
+
+            Employees = new List<Employee> { e1, e2, e3, e4 };
+        }
+    }
+}
